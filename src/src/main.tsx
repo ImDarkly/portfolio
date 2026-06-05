b6adbed820f5e37a804f5a68ad42c5c56459dd14
ImDarkly <svit.darkly@gmail.com>
2026-06-05 23:45:35 +0300

    Add animated favicon and update meta tags

--- src/src/main.tsx
+++ src/src/main.tsx
@@ -4,6 +4,9 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { startFaviconBlink } from "./favicon.ts"
+
+startFaviconBlink()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
